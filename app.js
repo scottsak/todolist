d60bef68a7af760e618b29b547e9f8f30e1a6943
scottsak <48599109+scottsak@users.noreply.github.com>
2021-08-04 00:16:36 -0700

    added html file

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ app.get("/", function(req, res){
   var today= new Date();
 
   if(today.getDay() === 6 || today.getDay() === 0){
-    res.send("Yay it's the weekend!");
+    res.write("<h1>Yay it's the weekend!</h1>");
   }
   else{
-    res.send("Boo! I have to work today");
+    res.write("<p>It is the work day.</p>");
+    res.write("<h1>Boo! I have to work today</h1>");
+    res.send();
   }
 });
 
