901a1d52fa9e11d109114c2af5330b8c6d3fd2e8
scottsak <48599109+scottsak@users.noreply.github.com>
2021-10-26 02:52:17 -0700

    partly added some style to site

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
