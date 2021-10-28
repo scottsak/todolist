165024ec16f17e6afa5ba4630426bbc6dc92a420
scottsak <48599109+scottsak@users.noreply.github.com>
2021-10-28 02:11:10 -0700

    added date object

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,27 +16,26 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-  var today= new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let day = date();
 
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {kindOfDay: day, newListItems:items})
+  res.render("list", {listTitle: day, newListItems:items})
 });
 
 app.post("/", function(req,res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
+  if(req.body.list === "Work"){
+      workItems.push(item);
+      res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/")
+  }
 
-  items.push(item)
+});
 
-  res.redirect("/");
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, function(){
---
+++ date.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+
+module.exports = getDate;
+
+function getDate() {
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
